66b70f110c6b27a96e475d76de02dfb334d9f85a
mariano <mariano.poletti93@gmail.com>
2022-06-18 11:06:56 -0300

    SimuladorInteractivo

---
+++ script.js
@@ -0,0 +1,18 @@
+let  sueldoNeto
+let alicuotaSinG = 0.89
+let alicuotaConG = 0.75
+
+do{
+    sueldoNeto= parseFloat(prompt("Ingresa Tu Sueldo Neto"))
+} while (
+    isNaN(sueldoNeto)
+)
+
+if (sueldoNeto <= 300000){
+    resultado = sueldoNeto*alicuotaSinG
+} else {
+    resultado = sueldoNeto*alicuotaConG
+}
+console.log(resultado)
+
+
