b5333762222e093750004c7240c7e0014ce37eb0
mariano <mariano.poletti93@gmail.com>
2022-06-20 21:38:03 -0300

    agregando funcion

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
 let  sueldoNeto
 let alicuotaSinG = 0.89
-let alicuotaConG = 0.75
+let alicuotaConG = 0.7
+
+function calcularSing (sueldoNeto, alicuotaSinG){
+    sueldoNeto*alicuotaSinG
+}
+function calcularConG (sueldoNeto, alicuotaConG){
+    sueldoNeto*alicuotaConG
+}
 
 do{
     sueldoNeto= parseFloat(prompt("Ingresa Tu Sueldo Neto"))
@@ -8,11 +15,22 @@ do{
     isNaN(sueldoNeto)
 )
 
-if (sueldoNeto <= 300000){
-    resultado = sueldoNeto*alicuotaSinG
-} else {
-    resultado = sueldoNeto*alicuotaConG
+//no se porque aca me dice undifined en el resultado 
+function calcularSing (sueldoNeto, alicuotaSinG){
+    sueldoNeto*alicuotaSinG
 }
+function calcularConG (sueldoNeto, alicuotaConG){
+    sueldoNeto*alicuotaConG
+}
+
+
+// cuando llamo a la funcion me dice que no esta definido 
+if (sueldoNeto <= 300000){
+    resultado = calcularSing (sueldoNeto, alicuotaSinG);
+} else {
+    resultado = calcularConG (sueldoNeto, alicuotaConG);
+}
+
 console.log(resultado)
 
 const contenerdor = document.getElementById("contenedor")
