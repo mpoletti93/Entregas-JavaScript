ad470336b7e6af289df727e12ea1621f87e77467
mariano <mariano.poletti93@gmail.com>
2022-07-10 10:45:28 -0300

    vuelvo a commit de git

--- data.js
+++ data.js
@@ -1,9 +1,23 @@
-//no pude lograr que se importen las variables 
-
-const products = [
-    {id:1, name:"REMERA", price: 100, stock:20},
-    {id:2, name:"PANTALON", price: 300, stock:10},
-    {id:3, name:"BUZO", price: 150, stock:15},
+const infoPersonas = [
+    {id: 1, nombre: "Juan", edad: 34, direccion: "Segui 2249", img:"juan.jpg", oficio: "Desarrollador FrontEnd"},
+    {id: 2, nombre: "Lucas", edad: 35, direccion: "Pichincha 49", img:"lucas.jpg", oficio: "Desarrollador BackEnd"},
+    {id: 3, nombre: "Bety", edad: 25, direccion: "Italia 345", img:"bety.jpg", oficio: "Tester QA"},
+    {id: 4, nombre: "Mariano", edad: 32, direccion: "España 1450", img:"mariano.jpg", oficio: "Almacenero"},
+    {id: 5, nombre: "Barbara", edad: 22, direccion: "Guatambu 2130", img:"barbara.jpg", oficio: "Bailarina"},
+    {id: 6, nombre: "Cristian", edad: 36, direccion: "Loria 768", img:"cristian.jpg", oficio: "Tecnico Reparador"},
+    {id: 7, nombre: "Pedro", edad: 28, direccion: "Laprida 489", img:"pedro.jpg", oficio: "Bombero"},
+    {id: 8, nombre: "Sofia", edad: 29, direccion: "Marmol 1980", img:"sofia.jpg", oficio: "Chef"},
+    {id: 9, nombre: "Cecilia", edad: 19, direccion: "Almirante Brown 1100", img:"cecilia.jpg", oficio: "Diseñadora Grafica"},
 ]
 
-export {products}
+const products = [
+    {id:1, name:"CERVEZA QUILMES", price: 125, stock:10},
+    {id:2, name:"FERNET BRANCA", price: 100, stock:12},
+    {id:3, name:"VINO COSECHA TARDIA", price: 67, stock:20},
+    {id:4, name:"WISKEY ", price: 200, stock:50},
+    {id:5, name:"RON BACARDI", price: 45, stock:70},
+    {id:6, name:"CERVEZA ANDES", price: 90, stock:90},
+    {id:7, name:"FERNET 1882", price: 67, stock:100}
+]
+
+export {infoPersonas, products};
--- script.js
+++ script.js
@@ -48,7 +48,6 @@ let sueldoNeto = 0
 let alicuotaSinG = 0.89
 let alicuotaConG = 0.7
 let resultado = 0 
-
 function calcularSing (sueldoNeto, alicuotaSinG){
     resultado = sueldoNeto*alicuotaSinG;
     return resultado 
@@ -58,17 +57,13 @@ function calcularConG (sueldoNeto, alicuotaConG){
     return  resultado 
 >>>>>>> PrimeEntregaProyecto
 }
-
 const persona1 = new Persona(1, "Francisco", "Pugh", 38, 12000)
 const persona2 = new Persona(2, "Dario", "Britos", 20, 22000)
 const persona3 = new Persona(3, "Alejandro", "Santos", 19, 17000)
 const persona4 = new Persona(4, "Gaston", "Sosa", 21, 13000)
 const persona5 = new Persona(5, "Maria", "Parodi", 19, 24000)
-
 const personas = [persona1, persona2, persona3, persona4, persona5]
-
 const divPersonas = document.getElementById('divPersonas')
-
 personas.forEach(persona => {
     divPersonas.innerHTML += `
         <div class="stylePersonas" id="persona${persona.id}">
@@ -79,28 +74,21 @@ personas.forEach(persona => {
         </div>
     `
 })
-
 const parrafo = document.createElement("p")
 parrafo.innerText = "Hola, como estan?"
 parrafo.id = "parrafo1"
-
 document.body.append(parrafo)
-
 document.getElementById('persona1').remove()
-
 <<<<<<< HEAD
 divPersonas.removeChild(document.getElementById('persona2'))
 =======
 const nombresTotales = nombres.concat(nombres2)
-
 let nombreEliminar = prompt("ingrese el nombre a eliminar").toLocaleLowerCase()
 let indice = nombresTotales.indexOf(nombreEliminar)
-
 if(indice >= 0){
     nombresTotales.splice(indice,1)
 } else {
     alert("Nombre no Encontrado")
 }
-
 console.log(nombresTotales)
 */
